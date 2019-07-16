a9981308d1fa5a7d837eb97adc8fb7cdeeba05f1
jovan <jovan.joksi@hotmail.com>
2019-07-16 23:57:36 +0200

    vanila js d3 Sequences sunburst

--- test.js
+++ test.js
@@ -1,3 +1,52 @@
+var colors = [
+  "#5687d1",
+  "#7b615c",
+  "#de783b",
+  "#6ab975",
+  "#a173d1",
+ "#bbbbbb"
+ ];
+ 
+class Ring{
+  constructor(id,occurrence,name){
+    this.occurrence = occurrence;
+    this.id = id;
+    this.name = name
+    this.children = [];
+    this.color = colors[Math.round(Math.random()*6)];
+  }
+
+  addChild(ring){
+      this.children.push(ring);
+  }
+  isLeaf(){
+      return this.children.length==0;
+  }
+
+}
+class RingBuilder{
+  constructor(data,originalCenter){
+    this.data=data
+    this.originalCenter=originalCenter;
+    var result = data.find(x=>x[0]==originalCenter);
+    if(!result)
+      throw "Center not found";
+    this.centerRing = new Ring(result[0],0,result[0])
+  }
+  build(center=this.originalCenter ,ring= this.centerRing){
+    var result = this.data.filter(x=> x[0]==center);
+    result.forEach(x=>{
+      var innerRing = new Ring(x[1],x[2],x[1])
+      this.build(x[1],innerRing);
+      ring.addChild(innerRing);
+    });
+    return ring;
+  }
+}
+
+
+
+
 // Dimensions of sunburst.
 var width = 750;
 var height = 600;
@@ -8,24 +57,8 @@ var b = {
   w: 75, h: 30, s: 3, t: 10
 };
 
-// Mapping of step names to colors.
-var colors = {
-  "home": "#5687d1",
-  "product": "#7b615c",
-  "search": "#de783b",
-  "account": "#6ab975",
-  "other": "#a173d1",
-  "end": "#bbbbbb"
-};
-
+// Mapping of step names to colors
 // Total size of all segments; we set this later, after loading the data.
-var data = [
-  [11,22,2],
-  [22,23,3],
-  [22,44,4]
-  [44,12,5]
-]
-
 var totalSize = 0; 
 var vis = d3.select("#chart").append("svg:svg")
     .attr("width", width)
@@ -45,18 +78,34 @@ var arc = d3.arc()
 
 // Use d3.text and d3.csvParseRows so that we do not need to have a header
 // row, and can receive the csv as an array of arrays.
-d3.text("visit-sequences.csv", function(text) {
-  var csv = d3.csvParseRows(text);
-  var json = buildHierarchy(csv);
-  createVisualization(json);
-});
+d3.text("data.csv", function(text) {
+  setTimeout(()=>{
+    var csv = d3.csvParseRows(text);
+    var builder = new RingBuilder(csv,csv[0][0])
+    var root = builder.build();
+    createVisualization(root);
+  },5000)
 
+});
+var data = [
+  [11,12,11],
+  [11,13,1],
+  [13,14,7],
+  [11,16,3],
+  [16,19,1],
+  [19,1122,3],
+  [1122,112,8],
+]
+
+//var builder = new RingBuilder(data,data[0][0])
+ // var root = builder.build();
+ // createVisualization(root);
 // Main function to draw and set up the visualization, once we have the data.
 function createVisualization(json) {
-
+ 
+  console.log(json)
   // Basic setup of page elements.
   initializeBreadcrumbTrail();
-  drawLegend();
   d3.select("#togglelegend").on("click", toggleLegend);
 
   // Bounding circle underneath the sunburst, to make it easier to detect
@@ -64,30 +113,32 @@ function createVisualization(json) {
   vis.append("svg:circle")
       .attr("r", radius)
       .style("opacity", 0);
-
+   d3.select("#selectedDiseases")
+      .text(json.name);
   // Turn the data into a d3 hierarchy and calculate the sums.
   var root = d3.hierarchy(json)
-      .sum(function(d) { return d.size; })
+      .sum(function(d) { return d.occurrence; })
       .sort(function(a, b) { return b.value - a.value; });
-  
+  console.log(root)
   // For efficiency, filter nodes to keep only those large enough to see.
   var nodes = partition(root).descendants()
       .filter(function(d) {
           return (d.x1 - d.x0 > 0.005); // 0.005 radians = 0.29 degrees
       });
-
+  console.log(nodes)
+  drawLegend(nodes);
   var path = vis.data([json]).selectAll("path")
       .data(nodes)
       .enter().append("svg:path")
       .attr("display", function(d) { return d.depth ? null : "none"; })
       .attr("d", arc)
       .attr("fill-rule", "evenodd")
-      .style("fill", function(d) { return colors[d.data.name]; })
+      .style("fill", function(d) { return d.data.color})
       .style("opacity", 1)
       .on("mouseover", mouseover);
 
   // Add the mouseleave handler to the bounding circle.
-  d3.select("#container").on("mouseleave", mouseleave);
+ // d3.select("#container").on("mouseleave", mouseleave);
 
   // Get total size of the tree = value of root node from partition.
   totalSize = path.datum().value;
@@ -189,7 +240,7 @@ function updateBreadcrumbs(nodeArray, percentageString) {
 
   entering.append("svg:polygon")
       .attr("points", breadcrumbPoints)
-      .style("fill", function(d) { return colors[d.data.name]; });
+      .style("fill", function(d) { return d.data.color; });
 
   entering.append("svg:text")
       .attr("x", (b.w + b.t) / 2)
@@ -217,37 +268,31 @@ function updateBreadcrumbs(nodeArray, percentageString) {
 
 }
 
-function drawLegend() {
-
-  // Dimensions of legend item: width, height, spacing, radius of rounded rect.
+function drawLegend(desc) {
   var li = {
     w: 75, h: 30, s: 3, r: 3
   };
-
   var legend = d3.select("#legend").append("svg:svg")
       .attr("width", li.w)
-      .attr("height", d3.keys(colors).length * (li.h + li.s));
-
+      .attr("height", desc.length * (li.h + li.s));
   var g = legend.selectAll("g")
-      .data(d3.entries(colors))
+      .data(d3.entries(desc))
       .enter().append("svg:g")
       .attr("transform", function(d, i) {
               return "translate(0," + i * (li.h + li.s) + ")";
            });
-
   g.append("svg:rect")
       .attr("rx", li.r)
       .attr("ry", li.r)
       .attr("width", li.w)
       .attr("height", li.h)
-      .style("fill", function(d) { return d.value; });
-
+      .style("fill", function(d) { return d.value.data.color; });
   g.append("svg:text")
       .attr("x", li.w / 2)
       .attr("y", li.h / 2)
       .attr("dy", "0.35em")
       .attr("text-anchor", "middle")
-      .text(function(d) { return d.key; });
+      .text(function(d) { return d.value.data.name; });
 }
 
 function toggleLegend() {
@@ -258,47 +303,3 @@ function toggleLegend() {
     legend.style("visibility", "hidden");
   }
 }
-
-// Take a 2-column CSV and transform it into a hierarchical structure suitable
-// for a partition layout. The first column is a sequence of step names, from
-// root to leaf, separated by hyphens. The second column is a count of how 
-// often that sequence occurred.
-function buildHierarchy(csv) {
-  var root = {"name": "root", "children": []};
-  for (var i = 0; i < csv.length; i++) {
-    var sequence = csv[i][0];
-    var size = +csv[i][1];
-    if (isNaN(size)) { // e.g. if this is a header row
-      continue;
-    }
-    var parts = sequence.split("-");
-    var currentNode = root;
-    for (var j = 0; j < parts.length; j++) {
-      var children = currentNode["children"];
-      var nodeName = parts[j];
-      var childNode;
-      if (j + 1 < parts.length) {
-   // Not yet at the end of the sequence; move down the tree.
- 	var foundChild = false;
- 	for (var k = 0; k < children.length; k++) {
- 	  if (children[k]["name"] == nodeName) {
- 	    childNode = children[k];
- 	    foundChild = true;
- 	    break;
- 	  }
- 	}
-  // If we don't already have a child node for this branch, create it.
- 	if (!foundChild) {
- 	  childNode = {"name": nodeName, "children": []};
- 	  children.push(childNode);
- 	}
- 	currentNode = childNode;
-      } else {
- 	// Reached the end of the sequence; create a leaf node.
- 	childNode = {"name": nodeName, "size": size};
- 	children.push(childNode);
-      }
-    }
-  }
-  return root;
-};
